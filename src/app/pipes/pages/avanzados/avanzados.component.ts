cca4b4c2c2f0172a485f364f10d0a48e5e3bea71
Isidro Moreno <imorenoh@jccm.es>
2022-04-27 13:19:56 +0200

    Pipes propios

--- src/app/pipes/pages/avanzados/avanzados.component.ts
+++ src/app/pipes/pages/avanzados/avanzados.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvanzadosComponent implements OnInit {
 
+  // Slice
+
+  alumnos = ['Pedro','Juan','Alicia','Isidro'];
+
+  coche = {
+    marca: 'Mercedes',
+    combustible: 'Gasolina',
+    anioMatriculacion: 2007
+  };
+
+  alumnosMap = {
+  '=0': 'No hay alumnos',
+  '=1': 'Hay un alumno',
+  'other': 'Hay # alumnos'
+
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  borrarAlumno() {
+    this.alumnos.pop();
+  }
+
 
 }
